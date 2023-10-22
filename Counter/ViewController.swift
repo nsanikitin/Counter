c1588d33fb95ba6a7cc6022a181de624c51bb1dc
Sergey <nsa010995@gmail.com>
2023-10-22 21:46:25 +0300

    Счетчик согласно заданию по спринту 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak private var value: UILabel!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
+    @IBOutlet weak private var toNillButton: UIButton!
+    @IBOutlet weak private var history: UITextView!
+    private var count: UInt = 0 //переменная для значения счетчика в label
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        history.isEditable = false
+        history.isScrollEnabled = true
     }
-
-
+    
+    //нажатие кнопки +
+    @IBAction private func buttonPlusDidTape(_ sender: Any) {
+        count += 1  //увеличиваем значение на 1
+        value.text = "Значение счетчика: \(count)"  //отображаем текст в label
+        history.insertText("\n \(Date().rusFormat): значение изменено на +1") //добавляем событие в историю изменений
+    }
+    
+    //нажатие кнопки -
+    @IBAction private func buttonMinusDidTape(_ sender: Any) {
+        if count > 0 {
+            count -= 1  //уменьшаем значение на -1
+            value.text = "Значение счетчика: \(count)"  //отображаем текст в label
+            history.insertText("\n \(Date().rusFormat): значение изменено на -1") //добавляем событие в историю изменений
+        } else {
+            history.insertText("\n \(Date().rusFormat): попытка уменьшить значение счётчика ниже 0") //добавляем событие в историю изменений
+        }
+    }
+    
+    //нажатие кнопки сброса
+    @IBAction private func buttonToNillDidTape(_ sender: Any) {
+        count = 0   //обнуляем значение счетчика
+        value.text = "Значение счетчика: \(count)"  //отображаем текст в label
+        history.insertText("\n \(Date().rusFormat): значение сброшено") //добавляем событие в историю изменений
+    }
 }
 
+//расширение для отображение даты нужном формате
+extension Date {
+    var rusFormat: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_RU")
+        dateFormatter.setLocalizedDateFormatFromTemplate("EE, dd MM yy, HH:mm:ss")
+        return dateFormatter.string(from:  self)
+    }
+    
+}
